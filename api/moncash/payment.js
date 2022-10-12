907ba5ce5e30dccf2f7f4c7e235c3390671e3c50
steevy <steevesanon6@gmail.com>
2022-10-12 17:54:33 -0400

    add function to convert gdes to dollars

--- api/moncash/payment.js
+++ api/moncash/payment.js
@@ -1,25 +1,50 @@
 const router = require('express').Router()
 const axios = require('axios')
+const ENPOINT_API='https://topup.pgecom.com/api/moncash/token'
 const USER_ID = process.env.USER_ID
-router.post('/', async (req, res) => {
+
+router.post('/', async(req, res) => {
     const { gdes } = req.body
-
-    console.log(req.body.gdes)
-
-
     await axios({
-        url: 'https://topup.pgecom.com/api/moncash/token',
+        url: ENPOINT_API,
         method: 'POST',
         data: {
-            gdes:gdes,
+            gdes,
             userID: USER_ID
         }
     }).then((reponse) => {
-        res.json(reponse.data)
+        res.status(200).json(reponse.data);
     }).catch((error) => {
-        res.json(error)
+        console.log(error);
+    })
+    
+})
+
+
+router.post('/dollartogdes',async(req,res)=>{
+    const {dollars}=req.body
+    //get value dollar 
+    const {data}=await axios({
+        url:'https://topup.pgecom.com/api/setting/globalsetting',
+        method:'GET'
     })
 
+    //store value
+    const moncashFees=data.monCashFees
+    //value + 10 %
+    const valueAfterCal=(dollars*moncashFees)*1.10
+    await axios({
+        url: ENPOINT_API,
+        method: 'POST',
+        data: {
+            gdes:valueAfterCal,
+            userID: USER_ID
+        }
+    }).then((reponse) => {
+        res.status(200).json(reponse.data);
+    }).catch((error) => {
+        console.log(error);
+    })
 })
 
 module.exports = router
